652cc5b3a2fbe841c975f7baab6aaa047a2b8cb4
moin21 <mamoinuddin@gmail.com>
2022-02-19 14:42:23 +0530

    Added: Logic for 1 day play calculating total profit

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,34 +1,41 @@
 package com.gamblingsimulator;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class GamblingSimulator {
-	public static int profit = 0;
+	public static final int STAKE_PER_DAY = 100;// STAKE PER DAY
+	public static final int BET_PER_GAME = 1;
+	public static final int WIN = 1;
+	public static int dailyStake = STAKE_PER_DAY;
+	public static int dailyProfit = 0;
+	public static int profit = 0;/// initial gambler bets with 1
 
 	public static void main(String[] args) {
-		/*
-		 * Taking user input from user for daily stack and bet amount.
-		 */
-		System.out.println("Welcome to Gambling Simulator Program!!");
-		System.out.println("Enter Daily Stack");
-		Scanner input = new Scanner(System.in);
-		int dailyStack = input.nextInt();
-		System.out.println("Enter your bet amount");
-		int betAmount = input.nextInt();
-		play(dailyStack, betAmount);
+		System.out.println("Welcome to Gambling Simulation");
+		dailyPlays();
 	}
 
-	public static void play(int dailyStack, int betAmount) {
+	public static void play() {
 		Random random = new Random();
 		int betReturns = random.nextInt(2);
-		if (betReturns == 1) {
-			profit++;
-
+		if (betReturns == WIN) {
+			profit += BET_PER_GAME;
+			dailyStake += BET_PER_GAME;
+			dailyProfit += BET_PER_GAME;// incrementing
 		} else {
-			profit--;
+			profit -= BET_PER_GAME;// decrementing
+			dailyStake -= BET_PER_GAME;
+			dailyProfit -= BET_PER_GAME;
 		}
-		System.out.println("Profit is = " + profit);
+		System.out.println("profit and ramaining stake = " + profit + "   " + dailyStake);
 
 	}
+
+	public static void dailyPlays() {
+		while (dailyStake > 50 && dailyStake < 150) {
+			play();
+		}
+		System.out.println(dailyProfit);
+	}
+
 }
