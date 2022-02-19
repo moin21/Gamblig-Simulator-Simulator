ded5e40768fe5bd0f761b224c464ec4e8340d0dc
moin21 <mamoinuddin@gmail.com>
2022-02-19 13:04:11 +0530

    Added: Logic for one round of betting

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,8 +1,10 @@
 package com.gamblingsimulator;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GamblingSimulator {
+	public static int profit = 0;
 
 	public static void main(String[] args) {
 		/*
@@ -12,8 +14,21 @@ public class GamblingSimulator {
 		System.out.println("Enter Daily Stack");
 		Scanner input = new Scanner(System.in);
 		int dailyStack = input.nextInt();
-		System.out.println("Enter the y- coordinate");
+		System.out.println("Enter your bet amount");
 		int betAmount = input.nextInt();
+		play(dailyStack, betAmount);
+	}
+
+	public static void play(int dailyStack, int betAmount) {
+		Random random = new Random();
+		int betReturns = random.nextInt(2);
+		if (betReturns == 1) {
+			profit++;
+
+		} else {
+			profit--;
+		}
+		System.out.println("Profit is = " + profit);
 
 	}
 }
