6e43a0f76a4f7c93f342f27f943e815c1614d385
moin21 <mamoinuddin@gmail.com>
2022-02-19 16:07:45 +0530

    Added: Logic for profit after 20 days of play

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,41 +1,61 @@
 package com.gamblingsimulator;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class GamblingSimulator {
-	public static final int STAKE_PER_DAY = 100;// STAKE PER DAY
 	public static final int BET_PER_GAME = 1;
 	public static final int WIN = 1;
-	public static int dailyStake = STAKE_PER_DAY;
-	public static int dailyProfit = 0;
-	public static int profit = 0;/// initial gambler bets with 1
+	public static int dailyStake;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation");
-		dailyPlays();
+		twentyDaysPlays();
 	}
 
+	/*
+	 * method for a single play to change daily stake
+	 */
 	public static void play() {
 		Random random = new Random();
 		int betReturns = random.nextInt(2);
 		if (betReturns == WIN) {
-			profit += BET_PER_GAME;
 			dailyStake += BET_PER_GAME;
-			dailyProfit += BET_PER_GAME;// incrementing
 		} else {
-			profit -= BET_PER_GAME;// decrementing
 			dailyStake -= BET_PER_GAME;
-			dailyProfit -= BET_PER_GAME;
 		}
-		System.out.println("profit and ramaining stake = " + profit + "   " + dailyStake);
 
 	}
-
+/*
+ * method for daily play, until user wins or lose 50 by calling play method 
+ */
 	public static void dailyPlays() {
+
 		while (dailyStake > 50 && dailyStake < 150) {
 			play();
 		}
-		System.out.println(dailyProfit);
-	}
 
+	}
+/*
+ *method for twenty day play and calculation of total profit or loss 
+ */
+	public static void twentyDaysPlays() {
+		int dailyProfit = 0;
+		int totalProfit = 0;
+		System.out.println("Enter daily stake");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		scanner.close();
+		for (int days = 1; days <= 20; days++) {
+			dailyStake = num;
+			dailyPlays();
+			dailyProfit = dailyStake - 100;
+			totalProfit += dailyProfit;
+			System.out.println("Profit for day " + days + " is: " + dailyProfit + " and total profit till date is: "
+					+ totalProfit);
+
+		}
+
+		System.out.println("Total profit of last 20 days: " + totalProfit);
+	}
 }
