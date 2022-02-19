b58fd74ac43510d0de7ec1f82c8fbef415e67c50
moin21 <mamoinuddin@gmail.com>
2022-02-19 12:55:13 +0530

    Added: Logic to take user input for Stack and Bet

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,7 +1,19 @@
 package com.gamblingsimulator;
 
+import java.util.Scanner;
+
 public class GamblingSimulator {
+
 	public static void main(String[] args) {
+		/*
+		 * Taking user input from user for daily stack and bet amount.
+		 */
 		System.out.println("Welcome to Gambling Simulator Program!!");
+		System.out.println("Enter Daily Stack");
+		Scanner input = new Scanner(System.in);
+		int dailyStack = input.nextInt();
+		System.out.println("Enter the y- coordinate");
+		int betAmount = input.nextInt();
+
 	}
 }
